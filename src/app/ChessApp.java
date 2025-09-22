84ac67cc2df2b76c7e3b401a125fa33f94a7e137
Luan Albis <luanalbis12@gmail.com>
2025-09-22 15:11:10 -0300

    First class: Position

--- src/app/ChessApp.java
+++ src/app/ChessApp.java
@@ -1,9 +1,13 @@
 package app;
 
+import boardgame.Position;
+
 public class ChessApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		Position p = new Position(2, 3);
+		System.out.println(p);
 
 	}
 
